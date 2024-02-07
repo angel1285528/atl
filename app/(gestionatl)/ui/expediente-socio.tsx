09199af95d0724aff36a5d2550461dbc84c884e2
angel1285528 <lce.angelrdz@gmail.com>
2024-02-06 19:44:42 -0600

    formulario para registrar socios habilitado, se trabaja en la página de expediente del cliente en la features sobre su diseño y formularios complementarios

--- app/(gestionatl)/ui/expediente-socio.tsx
+++ app/(gestionatl)/ui/expediente-socio.tsx
@@ -3,6 +3,7 @@
 // Importa los módulos y tipos necesarios
 import React, { useState, useEffect } from 'react';
 import { cargarCliente } from '@/app/lib/crud/cargarcliente';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 interface SocioData {
   id: string;
@@ -15,7 +16,8 @@ interface SocioData {
   scholarity: string | null;
   urlSocioPhoto: string | null;
   urlSocioIne: string | null;
-  rol: Role; // Asegúrate de importar o definir correctamente el tipo Role
+  rol: Role;
+  fechaRegistro: Date; // Asegúrate de importar o definir correctamente el tipo Role
 }
 
 interface ExpedienteSocioProps {
@@ -51,11 +53,29 @@ const ExpedienteSocio: React.FC<ExpedienteSocioProps> = ({ id }) => {
     }
 
     return (
-      <div>
+      <div className='flex'>
         {/* Aquí iría la renderización de los datos del socio */}
-        <h1>Datos del Socio</h1>
-        <h3>ID: {socio.id}</h3>
-        <p>Nombre: {socio.firstName} {socio.lastName}</p>
+        <div id='fotoSocio'>
+        <Avatar className=" size-35 w-40">
+                        <AvatarImage className=" border-4 hover:border-sky-900  rounded-full " src={socio.urlSocioPhoto} />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+          
+
+        </div>
+        <div id='Datos socios'>
+          <h1>Estatus: {socio.status}</h1>
+          <h2>Rol: {socio.rol}</h2>
+          <h3>ID: {socio.id}</h3>
+
+          <p>Nombre: {socio.firstName} {socio.lastName} {socio.secondLastName}</p>
+          <p>Email: {socio.email}</p>
+          <p>Celular: {socio.phoneNumber}</p>
+          <p>Trabajo: {socio.work}</p>
+          <p>Escolaridad: {socio.scholarity}</p>
+         {/* <p>Fecha de Registro: {socio.fechaRegistro}</p> */}
+
+          </div>
         {/* Continúa con el resto de los datos del socio */}
       </div>
     );
