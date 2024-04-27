aa40aee1e4d6ded5bbefbd5dfc5a8a05010452e7
angel1285528 <lce.angelrdz@gmail.com>
2024-04-27 16:51:23 -0600

    Se agrega enlaces funcionales a redes sociales y para descargas de fichas de inscripción

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -5,53 +5,55 @@ import { BsWhatsapp } from "react-icons/bs";
 import { MdOutlineEmail } from "react-icons/md";
 import { SiTiktok } from "react-icons/si";
 
-
-
 export default function Home(): React.JSX.Element {
   return (
-    <>
-      <main>
-      <div className='flex flex-col md:flex-row mb-auto md:pt-8'>
-  <div id="logo" className='md:flex-none mx-auto md:ml-8 pt-8 md:pt-8 m-4'>
-    <Image
-      src="/logo-academia.svg"
-      width={200}
-      height={200}
-      className='drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]'
-      priority={true}
-      alt='Logotipo Academia Tigres Linares'
-    />
-  </div>
-  <div id="titulo" className='flex-initial text-indigo-800 md:pt-8'>
-    <h1 className='md:text-9xl text-left antialiased font-bold tracking tracking-wide drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]'>ACADEMIA</h1>
-  </div>
-</div>
-
-        <div>
-          <h2 className='md:text-9xl md:pt-8  md:ml-8 text-center antialiased font-bold tracking tracking-wide drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]'>TIGRES LINARES</h2>
-          <div id="sitio-en-construccion" className='mx-auto'>
-            <h3 className=' text-4xl antialiased font-bold text-white'>Sitio web en construcción</h3>
-          </div>
-          <div id="icons" className='flex flex-row items-stretch mx-auto'>
-            <a href="https://www.facebook.com">
-              <TiSocialFacebookCircular className="mx-auto size-16" />
-            </a>
-            <a href="https://www.facebook.com">
-              <TiSocialInstagramCircular className="mx-auto size-16" />
-            </a>
-            <a href="https://www.facebook.com" className='align-middle object-scale-down'>
-              <BsWhatsapp className="mx-auto size-14 pt-2 object-scale-down" />
-            </a>
-            <a href="https://www.facebook.com">
-              <MdOutlineEmail className="mx-auto size-16" />
-            </a>
-            <a href="https://www.facebook.com">
-              <SiTiktok className="mx-auto size-14 pt-2 object-scale-down" />
-            </a>
-
-          </div>
+    <main>
+      
+      <div id='content' className='flex flex-col mb-auto'>
+        <div id="logo" className='mx-auto pt-8 m-2'>
+          <Image
+            src="/logo-academia.svg"
+            width={200}
+            height={200}
+            className='drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]'
+            priority={true}
+            alt='Logotipo Academia Tigres Linares'
+          />
         </div>
-      </main>
-    </>
+        <div id="titulo" className='flex-initial text-indigo-800'>
+          <h1 className='md:text-7xl text-center antialiased font-bold tracking tracking-wide drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]'>ACADEMIA</h1>
+        </div>
+
+        <div className='mx-auto text-center'>
+        <h2 className='md:text-7xl text-indigo-800 md:ml-8 text-center antialiased font-bold tracking tracking-wide drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]'>TIGRES LINARES</h2>
+        <div id="sitio-en-construccion" className='mx-auto text-center'>
+          <h3 className='text-4xl antialiased font-bold text-white'>Sitio web en construcción</h3>
+        </div>
+        </div>
+        <div id="icons" className='flex justify-center items-center mx-auto'>
+          <a href="https://facebook.com/tigres.linares.oficial">
+            <TiSocialFacebookCircular className="size-16" />
+          </a>
+          <a href="https://www.instagram.com/academiatigreslinares/">
+            <TiSocialInstagramCircular className="size-16" />
+          </a>
+          {/*} <a href="https://www.facebook.com">
+            <BsWhatsapp className="size-14 pt-2" />
+          </a> */}
+          <a href="mailto:info@tigreslinares.com">
+            <MdOutlineEmail className="size-16" />
+          </a>
+          {/* <a href="https://www.facebook.com">
+            <SiTiktok className="size-14 pt-2" />
+          </a>*/}
+        </div>
+        <div id='documentos' className='text-center text-blue-800 text-xl font-bold'>
+          <h3><a href="https://drive.google.com/file/d/1R-6RPy4OzMuq-zDx_vCpX_H_7Iqe8VxA/view?usp=drive_link">Descargar Ficha de Datos Familiares</a></h3>
+          <h3><a href="https://drive.google.com/file/d/1oG1Wh0L4w8oEbPJTDarOQ8wypny7DmsP/view?usp=drive_link">Descargar Ficha de Inscripción de Jugadores</a></h3>
+          
+        </div>
+          </div>
+    </main>
   )
 }
+
